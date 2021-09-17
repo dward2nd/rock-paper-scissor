d4627ef96c19494fc66e61efbf2b39c5b0cc8a37
Saksinkarn Petchkuljinda <=>
2021-09-18 03:02:36 +0700

    Added the ability to HTTP Post / Use Main activity to manage overall process from now on

--- app/src/main/java/com/rockpaperscissor/LoginActivity.java
+++ app/src/main/java/com/rockpaperscissor/LoginActivity.java
@@ -4,24 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
-   public static final String EXTRA_MESSAGE = "com.rockpaperscissor.MAIN";
+   public static final String EXTRA_MESSAGE = "com.rockpaperscissor.LOGIN";
+   public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+   public final String postUrl = "http://192.168.1.8:3000";
+   public final String postBody = "{\n" +
+         "    \"name\": \"morpheus\",\n" +
+         "    \"job\": \"leader\"\n" +
+         "}";
+   private long pressedTime;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+      setContentView(R.layout.activity_login);
       getSupportActionBar().setDisplayShowTitleEnabled(false);
+      //Intent intent = getIntent();
+   }
+
+   @Override
+   public void onBackPressed() {
+      if (pressedTime + 2000 > System.currentTimeMillis()) {
+         Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+         homeIntent.addCategory(Intent.CATEGORY_HOME);
+         homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+         startActivity(homeIntent);
+      } else {
+         Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+      }
+      pressedTime = System.currentTimeMillis();
    }
 
    /**
     * Event handler when user tap the send button
     **/
    public void onNextBtnClicked(View view) {
-      // try connecting to the server.
+      // connect to the server
+      try {
+         postRequest(postUrl, postBody);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
 
       // launch new activity.
       Intent intent = new Intent(this, SelectPlayer.class);
@@ -29,5 +67,26 @@ public class LoginActivity extends AppCompatActivity {
       String message = editText.getText().toString();
       intent.putExtra(EXTRA_MESSAGE, message);
       startActivity(intent);
+   }
+
+   void postRequest(String postUrl, String postBody) throws IOException {
+      OkHttpClient client = new OkHttpClient();
+      RequestBody body = RequestBody.create(JSON, postBody);
+      Request request = new Request.Builder()
+            .url(postUrl)
+            .post(body)
+            .build();
+
+      client.newCall(request).enqueue(new Callback() {
+         @Override
+         public void onFailure(Call call, IOException e) {
+            call.cancel();
+         }
+
+         @Override
+         public void onResponse(Call call, Response response) throws IOException {
+            Log.e("TAG", response.body().string());
+         }
+      });
    }
 }
---
+++ app/src/main/java/com/rockpaperscissor/MainActivity.java
@@ -0,0 +1,35 @@
+package com.rockpaperscissor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+      // in case not.
+      Intent loginIntent = new Intent(this, LoginActivity.class);
+      startActivity(loginIntent);
+   }
+
+   /*
+   @Override
+   public void onBackPressed() {
+      if (pressedTime + 2000 > System.currentTimeMillis()) {
+         this.finish();
+         System.exit(0);
+      } else {
+         Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+      }
+      pressedTime = System.currentTimeMillis();
+   }
+
+    */
+}
